af8848eb1b5988ee08c07c8c24d0d3d3ccc139b5
Tusshar-developer <67571309+Tusshar-developer@users.noreply.github.com>
2020-09-13 00:13:23 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
+function setup() {
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(10);
+  snake = new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  background(0);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    document.querySelector('#game-over').textContent = 'Game Over!';
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(0, 255, 0);
+  rect(food.x, food.y, 1, 1);
+}
